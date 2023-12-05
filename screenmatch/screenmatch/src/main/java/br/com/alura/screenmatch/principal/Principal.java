ee9c24d17f4f3987a0e4e3f5b3b2755d8ba6e672
dadderio <dadderio79@outlook.com>
2023-12-05 07:25:52 -0300

    update principal

--- screenmatch/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ screenmatch/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -33,9 +33,16 @@ public class Principal {
             DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
             temporadas.add(dadosTemporada);
         }
-
         temporadas.forEach(System.out::println);
 
+//        for(int i=0; i<dados.totalTemporadas(); i++){
+//            List<DadosEpisodio> episodioTemporada = temporadas.get(i).episodios();
+//            for(int j = 0; j< dados.totalTemporadas(); j++){
+//                System.out.println(episodioTemporada.get(j).titulo());
+//            }
+//        }
+
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
 
 
     }
